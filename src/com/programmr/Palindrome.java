1d29b96746d14831c251abc3d3fbda7d55f8801e
northshaman <north-shaman@mail.ru>
2018-07-03 16:37:43 +0500

    Palindrome class

--- src/com/programmr/Palindrome.java
+++ src/com/programmr/Palindrome.java
@@ -1,12 +1,32 @@
 package com.programmr;
 
+import java.util.Scanner;
+
 /**
- * Created by shaman on 03.07.2018.
+ * Write a program which takes input as string and check if it is palindrome or not.If the string and the reverse of string are same then we say string is palindrome.
+ * <p>
+ * If string is "level" then the output should be: palindrome
  */
 public class Palindrome {
 
-    public static void main(String[] args) {
-        System.out.println("Palindrome in main branch!");
+    private static boolean stringIsPolyndrome(String input) {
+        char[] stringArray = input.toCharArray();
+        for (int i = 0; i < stringArray.length / 2; i++) {
+            if (stringArray[i] != stringArray[stringArray.length - i - 1])
+            return false;
+        }
+        return true;
     }
 
+    public static void main(String[] args) {
+        System.out.println("Please enter the word: ");
+        String userWord;
+        while (true) {
+            userWord = new Scanner(System.in).next();
+            if (userWord.length() > 0)
+                stringIsPolyndrome(userWord);
+            System.out.println(stringIsPolyndrome(userWord));
+            break;
+        }
+    }
 }
