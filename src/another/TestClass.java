6a48c81b991708be396330ccc815782c8200cf1f
A.Sheptikin <north-shaman@mail.ru>
2018-07-15 12:54:33 +0500

    add new class for test merge

---
+++ src/another/TestClass.java
@@ -0,0 +1,11 @@
+package another;
+
+/**
+ * Created by user on 15.07.2018.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+}
